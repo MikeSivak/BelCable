35da73206b19296c8d3fd0b17c5086314a9858a9
MikeSivak <noizemcnorm@gmail.com>
2021-08-17 01:27:40 +0300

    add dinamic data on product page

--- src/components/ProductPage/ProductPage.js
+++ src/components/ProductPage/ProductPage.js
@@ -1,48 +1,46 @@
 import './ProductPage.css'
 import { Card, Col, Row, Dropdown, Tabs, Tab } from 'react-bootstrap'
 
-const industryNames = [
-    'Энергетика',
-    'Строительство',
-    'Металлургия',
-    'Нефтегазовая промышленность',
-    'Транспортная инфраструктура',
-    'Машиностроение'
-]
 
-const industryImages = [
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-]
 
-const purposeNames = [
-    'Кабели и провода монтажные',
-    'Кабели и провода связи',
-    'Кабели управления и передачи данных',
-    'Кабели силовые',
-    'Кабели и провода для сигнализации и блокировки',
-    'Кабели контрольные',
-    'Кабели комбинированные',
-    'Провода и шнуры соединительные',
-    'Кабели и многожильные подвесные',
-    'Кабели, провода, шнуры микрофонные',
-    'Провода автотракторные',
-    'Провода бортовые облученные',
-    'Провода для промышленных взрывных работ',
-    'Провода для щёток электрических машин, провода для платы',
-    'Провода контактные для экранизирующих элементов',
-    'Провода кроссовые станционные с изоляцией из ПВХ пластика',
-    'Провода обмоточные для погруженных электродвигателей',
-    'Провода трансляционные с пластмассовой изоляцией',
-    'Провода установочные для водопогружных электродвигателей',
-    'Радиочастотные кабели'
-]
+// const industryImages = [
+
+// ]
 
 export default function ProductPage() {
+
+    const industry = [
+        { name: 'Энергетика', image: 'tow.png' },
+        { name: 'Строительство', image: 'crane.png' },
+        { name: 'Коммуникации', image: 'phone.png' },
+        { name: 'Нефтегазовая промышленность', image: 'platform.png' },
+        { name: 'Транспортная инфраструктура', image: 'train.png' },
+        { name: 'Машиностроение', image: 'car.png' },
+    ]
+
+    const purpose = [
+        { name: 'Кабели и провода монтажные' },
+        { name: 'Кабели и провода связи' },
+        { name: 'Кабели управления и передачи данных' },
+        { name: 'Кабели силовые' },
+        { name: 'Кабели и провода для сигнализации и блокировки' },
+        { name: 'Кабели контрольные' },
+        { name: 'Кабели комбинированные' },
+        { name: 'Провода и шнуры соединительные' },
+        { name: 'Кабели и многожильные подвесные' },
+        { name: 'Кабели, провода, шнуры микрофонные' },
+        { name: 'Провода автотракторные' },
+        { name: 'Провода бортовые облученные' },
+        { name: 'Провода для промышленных взрывных работ' },
+        { name: 'Провода для щёток электрических машин, провода для платы' },
+        { name: 'Провода контактные для экранизирующих элементов' },
+        { name: 'Провода кроссовые станционные с изоляцией из ПВХ пластика' },
+        { name: 'Провода обмоточные для погруженных электродвигателей' },
+        { name: 'Провода трансляционные с пластмассовой изоляцией' },
+        { name: 'Провода установочные для водопогружных электродвигателей' },
+        { name: 'Радиочастотные кабели' }
+    ]
+
     return (
         <>
             <div className='product-page-container'>
@@ -51,11 +49,11 @@ export default function ProductPage() {
                         <Col md={4}>
                             <Tabs defaultActiveKey="byIndustry" id="uncontrolled-tab-example">
                                 <Tab eventKey="byIndustry" title="По отраслям" className=''>
-                                    {industryNames.map((names) => (
+                                    {industry.map((data) => (
                                         <>
                                             <Dropdown >
                                                 <Dropdown.Toggle className='drop-buttons mt-1' id="dropdown-basic">
-                                                    {names}
+                                                    {data.name}
                                                 </Dropdown.Toggle>
 
                                                 <Dropdown.Menu>
@@ -65,14 +63,15 @@ export default function ProductPage() {
                                                 </Dropdown.Menu>
                                             </Dropdown>
                                         </>
-                                    ))}
+                                    ))
+                                    }
                                 </Tab>
                                 <Tab eventKey="byPurpose" title="По назначению">
-                                    {purposeNames.map((names) => (
+                                    {purpose.map((data) => (
                                         <>
                                             <Dropdown >
                                                 <Dropdown.Toggle className='drop-buttons mt-1' id="dropdown-basic">
-                                                    {names}
+                                                    {data.name}
                                                 </Dropdown.Toggle>
 
                                                 <Dropdown.Menu>
@@ -82,27 +81,52 @@ export default function ProductPage() {
                                                 </Dropdown.Menu>
                                             </Dropdown>
                                         </>
-                                    ))}
+                                    ))
+                                    }
                                 </Tab>
                             </Tabs>
                         </Col>
                         <div class="col-md-8 products">
-                            <Row xs={1} md={3} className="g-4">
-                                {industryNames.map((names) => (
-                                    <Col>
-                                        <Card className='product'>
-                                            <Card.Img className='cardImg' variant="top" src="itemCable.png" />
-                                            <Card.Body>
-                                                <Card.Title>{names}</Card.Title>
-                                                <Card.Text>
-                                                    This is a longer card with supporting text below as a natural
-                                                    lead-in to additional content. This content is a little bit longer.
-                                                </Card.Text>
-                                            </Card.Body>
-                                        </Card>
-                                    </Col>
-                                ))}
-                            </Row>
+                            <Tabs defaultActiveKey="byIndustry" id="uncontrolled-tab-example">
+                                <Tab eventKey="byIndustry" title="По отраслям" className=''>
+                                    <Row xs={1} md={3} className="g-4">
+                                        {industry.map((data) => (
+                                            <Col>
+                                                <Card className='product'>
+                                                    <Card.Img className='cardImg' variant="top" src={data.image} />
+                                                    <Card.Body>
+                                                        <Card.Title>{data.name}</Card.Title>
+                                                        <Card.Text>
+                                                            This is a longer card with supporting text below as a natural
+                                                            lead-in to additional content. This content is a little bit longer.
+                                                        </Card.Text>
+                                                    </Card.Body>
+                                                </Card>
+                                            </Col>
+                                        ))
+                                        }
+                                    </Row>
+                                </Tab>
+                                <Tab eventKey="byPurpose" title="По назначению">
+                                    <Row xs={1} md={3} className="g-4">
+                                        {purpose.map((data) => (
+                                            <Col>
+                                                <Card className='product'>
+                                                    <Card.Img className='cardImg' variant="top" src='itemCable.png' />
+                                                    <Card.Body>
+                                                        <Card.Title>{data.name}</Card.Title>
+                                                        <Card.Text>
+                                                            This is a longer card with supporting text below as a natural
+                                                            lead-in to additional content. This content is a little bit longer.
+                                                        </Card.Text>
+                                                    </Card.Body>
+                                                </Card>
+                                            </Col>
+                                        ))
+                                        }
+                                    </Row>
+                                </Tab>
+                            </Tabs>
                         </div>
                     </div>
                 </div>
