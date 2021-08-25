0952a302bd78847ec8eed60a00aacbe482a40e75
MikeSivak <noizemcnorm@gmail.com>
2021-08-25 21:36:49 +0300

    update a search field

--- src/components/HomePage/ProductCarousel/ProductCarousel.js
+++ src/components/HomePage/ProductCarousel/ProductCarousel.js
@@ -4,32 +4,33 @@ import './Carousel.css'
 export default function ProductCarousel() {
     return (
         <>
-            <div className='productLabel'>
-                <p>ПРОДУКЦИЯ</p>
-            </div>
-            <Carousel className='product-carousel'>
-                <Carousel.Item interval={1000} className='product-carousel-item'>
-                    <Row xs={1} md={4} className="g-4">
-                        {Array.from({ length: 8 }).map((_, idx) => (
-                            <Col className='product-carousel-col'>
-                                <Card className='product-carousel-card'>
-                                    <Image className='product-carousel-round round-image' src='round1.png' alt='img'/>
-                                    <Image className='product-carousel-round-2 round-image' src='round2.png' alt='img'/>
-                                    <Card.Body>
-                                        <Card.Title><span className='card-title'>КАБЕЛИ И ПРОВОДА МОНТАЖНЫЕ</span></Card.Title>
-                                        <Card.Text>
-                                            <span className='card-text'>Объединяют специальную группу кабельной продукции, предназначенной для передачи электрической энергии в пределах одной электрической установки, электрического прибора или аппарата.</span>
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                        ))}
-                    </Row>
-                    <Carousel.Caption>
+            <div className='product-carousel-container'>
+                <div className='productLabel'>
+                    <p>ПРОДУКЦИЯ</p>
+                </div>
+                <Carousel className='product-carousel'>
+                    <Carousel.Item interval={1000} className='product-carousel-item'>
+                        <Row xs={1} md={4} className="g-4">
+                            {Array.from({ length: 8 }).map((_, idx) => (
+                                <Col className='product-carousel-col'>
+                                    <Card className='product-carousel-card'>
+                                        <Image className='product-carousel-round round-image' src='round1.png' alt='img' />
+                                        <Image className='product-carousel-round-2 round-image' src='round2.png' alt='img' />
+                                        <Card.Body>
+                                            <Card.Title><span className='card-title'>КАБЕЛИ И ПРОВОДА МОНТАЖНЫЕ</span></Card.Title>
+                                            <Card.Text>
+                                                <span className='card-text'>Объединяют специальную группу кабельной продукции, предназначенной для передачи электрической энергии в пределах одной электрической установки, электрического прибора или аппарата.</span>
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            ))}
+                        </Row>
+                        <Carousel.Caption>
 
-                    </Carousel.Caption>
-                </Carousel.Item>
-                {/* <Carousel.Item interval={1000}>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    {/* <Carousel.Item interval={1000}>
                     <Row xs={1} md={4} className="g-4">
                         {Array.from({ length: 8 }).map((_, idx) => (
                             <Col>
@@ -69,9 +70,10 @@ export default function ProductCarousel() {
 
                     </Carousel.Caption>
                 </Carousel.Item> */}
-            </Carousel>
-            <div className='catalogButton'>
-                <Button className='cB' variant="danger">Перейти в каталог</Button>
+                </Carousel>
+                <div className='catalogButton'>
+                    <Button className='cB' variant="danger">Перейти в каталог</Button>
+                </div>
             </div>
         </>
     )
--- src/components/HomePage/Search/Search.js
+++ src/components/HomePage/Search/Search.js
@@ -1,13 +1,19 @@
-import { Button } from 'react-bootstrap'
+import { Row, Col } from 'react-bootstrap'
 import './Search.css'
 
 export default function Search() {
     return (
         <>
-            <div className='searchBlock'>
-                <img className='searchImage' src='search.png' alt='' />
-                <input type='text' placeholder='Поиск по марке кабеля' className='seacrhInput'></input>
-                <Button className='searchButton' variant='danger'>Искать</Button>
+            <div className='search-container'>
+                <img className='search-image' src='search-img.png' alt='' />
+                <Row className='search-inputs'>
+                    <Col className='col-md-8'>
+                        <input type='text' placeholder='Поиск по марке кабеля' className='seacrh-text-block'></input>
+                    </Col>
+                    <Col className='col-md-2'>
+                        <button className='search-button' variant='danger'>Искать</button>
+                    </Col>
+                </Row>
             </div>
         </>
     )
