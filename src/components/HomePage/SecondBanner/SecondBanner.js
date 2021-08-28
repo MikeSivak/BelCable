6f7bef63190375dbf12a08d8ece6e8bdbe0f3524
MikeSivak <noizemcnorm@gmail.com>
2021-08-28 15:18:07 +0300

    fixed scrolling bugs in second banner

--- src/components/HomePage/SecondBanner/SecondBanner.js
+++ src/components/HomePage/SecondBanner/SecondBanner.js
@@ -13,31 +13,27 @@ export default function SecondBanner() {
                             <Row className='second-banner-label'>Преимущества</Row>
                         </Col>
                     </Row>
-                    <Row className='second-banner-grid-1'>
+                    <br></br>
+                    <Row className='second-banner-common-grid'>
                         <Col>
                             <Row className='label-amount'>100 293</Row>
                             <Row className='label-value'>киллометров</Row>
                             <Row className='value-description'>кабельной продукции высокого качества реализовано в 2020 году</Row>
-                        </Col>
-                        <Col>
+                            <br></br>
                             <Row className='label-amount'>1859</Row>
                             <Row className='label-value'>тон</Row>
                         </Col>
                         <Col>
                             <Row className='label-amount'>30 000</Row>
                             <Row className='label-value'>киллометров</Row>
-                        </Col>
-                    </Row>
-                    <Row className='second-banner-grid-2'>
-                        <Col>
+                            <br></br>
                             <Row className='label-amount'>683</Row>
                             <Row className='label-value'>сотрудников</Row>
                         </Col>
                         <Col>
                             <Row className='label-amount'>345</Row>
                             <Row className='label-value'>модернизированного оборудования</Row>
-                        </Col>
-                        <Col>
+                            <br></br>
                             <Row className='label-amount'>867</Row>
                             <Row className='label-value'>инновационная продукция</Row>
                         </Col>
