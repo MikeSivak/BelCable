108f2a56e7d1bc336660894b5d2b841bac0648cc
MikeSivak <noizemcnorm@gmail.com>
2021-08-28 16:25:45 +0300

    change layout of industrial list

--- src/components/HomePage/IndustriesList/IndustriesList.js
+++ src/components/HomePage/IndustriesList/IndustriesList.js
@@ -43,7 +43,7 @@ export default function IndustriesList() {
                 <div className='industry-label'>
                     По отраслям
                 </div>
-                <Row xs={1} md={2} className="g-2 industry-list-row">
+                {/* <Row xs={1} md={2} className="g-2 industry-list-row">
                     <Col className='industry-list-col-blocks'>
                         <Row md={3} className='industry-list-row-blocks'>
                             <Col>
@@ -136,90 +136,105 @@ export default function IndustriesList() {
                             </Col>
                         </Row>
                     </Col>
-                    {/* <Col>
-                        <Link className='custom-link' to='/product'>
-                            <Card className='industry-product-card'>
-                                <Image className='industry-product-card-circle' src='circle.png' alt='img' />
-                                <Image className='industry-product-card-img-1 ipc-img' src='industryTower.png' alt='img' />
-                                <Card.Body>
-                                    <Card.Title><span className='product-name'>Энергетика</span></Card.Title>
-                                    <Card.Text>
-                                        Продукция для строительства и реконструкции кабельных линий
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Link>
+                </Row> */}
+
+                <Row xs={1} md={3} className="g-2 industry-list-row">
+                    <Col className='industry-list-col-blocks'>
+                        <Row md={2} className='industry-list-row-blocks'>
+                            <Col>
+                                <Link className='custom-link' to='/product'>
+                                    <Card className='industry-product-card'>
+                                        <Image className='industry-product-card-circle' src='circle.png' alt='img' />
+                                        <Image className='industry-product-card-img-1 ipc-img' src='industryTower.png' alt='img' />
+                                        <Card.Body>
+                                            <Card.Title><span className='product-name'>Энергетика</span></Card.Title>
+                                            <Card.Text>
+                                                <span className='card-text'>Продукция для строительства и реконструкции кабельных линий</span>
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                </Link>
+                            </Col>
+                            <Col>
+                                <Link className='custom-link' to='/product'>
+                                    <Card className='industry-product-card'>
+                                        <Image className='industry-product-card-circle' src='circle.png' alt='img' />
+                                        <Image className='industry-product-card-img-2 ipc-img' src='industryCrane.png' alt='img' />
+                                        <Card.Body>
+                                            <Card.Title><span className='product-name'>Строительство</span></Card.Title>
+                                            <Card.Text>
+                                                <span className='card-text'>Продукция для энергоснабжения объектов гражданского</span>
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                </Link>
+                            </Col>
+                        </Row>
                     </Col>
-                    <Col>
-                        <Link className='custom-link' to='/product'>
-                            <Card className='industry-product-card'>
-                                <Image className='industry-product-card-circle' src='circle.png' alt='img' />
-                                <Image className='industry-product-card-img-2 ipc-img' src='industryCrane.png' alt='img' />
-                                <Card.Body>
-                                    <Card.Title><span className='product-name'>Строительство</span></Card.Title>
-                                    <Card.Text>
-                                        Продукция для энергоснабжения объектов гражданского
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Link>
+                    <Col className='industry-list-col-blocks'>
+                        <Row md={2} className='industry-list-row-blocks'>
+                            <Col>
+                                <Link className='custom-link' to='/product'>
+                                    <Card className='industry-product-card'>
+                                        <Image className='industry-product-card-circle' src='circle.png' alt='img' />
+                                        <Image className='industry-product-card-img-3 ipc-img' src='industryPhone.png' alt='img' />
+                                        <Card.Body>
+                                            <Card.Title><span className='product-name'>Коммуникации</span></Card.Title>
+                                            <Card.Text>
+                                                <span className='card-text'>Продукция общепромышленного назначения</span>
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                </Link>
+                            </Col>
+                            <Col>
+                                <Link className='custom-link' to='/product'>
+                                    <Card className='industry-product-card'>
+                                        <Image className='industry-product-card-circle' src='circle.png' alt='img' />
+                                        <Image className='industry-product-card-img-4 ipc-img' src='industryPlatform.png' alt='img' />
+                                        <Card.Body>
+                                            <Card.Title><span className='product-name'>Нефтегазовая промышленность</span></Card.Title>
+                                            <Card.Text>
+                                                <span className='card-text'>Кабели для питания насосов в нефтескважинах</span>
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                </Link>
+                            </Col>
+                        </Row>
                     </Col>
-                    <Col>
-                        <Link className='custom-link' to='/product'>
-                            <Card className='industry-product-card'>
-                                <Image className='industry-product-card-circle' src='circle.png' alt='img' />
-                                <Image className='industry-product-card-img-3 ipc-img' src='industryPhone.png' alt='img' />
-                                <Card.Body>
-                                    <Card.Title><span className='product-name'>Коммуникации</span></Card.Title>
-                                    <Card.Text>
-                                        Продукция общепромышленного назначения
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Link>
-                    </Col> */}
-                    {/* <Col>
-                        <Link className='custom-link' to='/product'>
-                            <Card className='industry-product-card'>
-                                <Image className='industry-product-card-circle' src='circle.png' alt='img' />
-                                <Image className='industry-product-card-img-4 ipc-img' src='industryPlatform.png' alt='img' />
-                                <Card.Body>
-                                    <Card.Title><span className='product-name'>Нефтегазовая промышленность</span></Card.Title>
-                                    <Card.Text>
-                                        Кабели для питания насосов в нефтескважинах
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Link>
+                    <Col className='industry-list-col-blocks'>
+                        <Row md={2} className='industry-list-row-blocks'>
+                            <Col>
+                                <Link className='custom-link' to='/product'>
+                                    <Card className='industry-product-card'>
+                                        <Image className='industry-product-card-circle' src='circle.png' alt='img' />
+                                        <Image className='industry-product-card-img-5 ipc-img' src='iTrain.png' alt='img' />
+                                        <Card.Body>
+                                            <Card.Title><span className='product-name'>Транспортная инраструктура</span></Card.Title>
+                                            <Card.Text>
+                                                <span className='card-text'>Для внутренних и наружных соединений подвижного состава рельсового транспорта и троллейбусов</span>
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                </Link>
+                            </Col>
+                            <Col>
+                                <Link className='custom-link' to='/product'>
+                                    <Card className='industry-product-card'>
+                                        <Image className='industry-product-card-circle' src='circle.png' alt='img' />
+                                        <Image className='industry-product-card-img-6 ipc-img' src='industryCar.png' alt='img' />
+                                        <Card.Body>
+                                            <Card.Title><span className='product-name'>Машиностроение</span></Card.Title>
+                                            <Card.Text>
+                                                <span className='card-text'>Продукция для энергоснабжения инфраструктуры предприятия</span>
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                </Link>
+                            </Col>
+                        </Row>
                     </Col>
-                    <Col>
-                        <Link className='custom-link' to='/product'>
-                            <Card className='industry-product-card'>
-                                <Image className='industry-product-card-circle' src='circle.png' alt='img' />
-                                <Image className='industry-product-card-img-5 ipc-img' src='iTrain.png' alt='img' />
-                                <Card.Body>
-                                    <Card.Title><span className='product-name'>Транспортная инраструктура</span></Card.Title>
-                                    <Card.Text>
-                                        Для внутренних и наружных соединений подвижного состава рельсового транспорта и троллейбусов
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Link>
-                    </Col>
-                    <Col>
-                        <Link className='custom-link' to='/product'>
-                            <Card className='industry-product-card'>
-                                <Image className='industry-product-card-circle' src='circle.png' alt='img' />
-                                <Image className='industry-product-card-img-6 ipc-img' src='industryCar.png' alt='img' />
-                                <Card.Body>
-                                    <Card.Title><span className='product-name'>Машиностроение</span></Card.Title>
-                                    <Card.Text>
-                                        Продукция для энергоснабжения инфраструктуры предприятия
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Link>
-                    </Col> */}
                 </Row>
             </div>
         </>
