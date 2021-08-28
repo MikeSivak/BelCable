def73ac4f404d2eaeb430e546933d4bef569cd13
MikeSivak <noizemcnorm@gmail.com>
2021-08-28 14:21:46 +0300

    changed font styles in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -18,12 +18,12 @@ export default function Footer() {
                                 <h6 class="text-uppercase fw-bold mb-4" style={{ color: 'red' }}>
                                     <img height='37px' width='309px' src='lbl.png' />
                                 </h6>
-                                <p>
+                                <p className='belcable-history'>
                                     Более 50 лет ОАО "Беларускабель" - одно из ведущих кабельных предприятий в Республике Беларусь, которое изготавливает и поставляет кабельную продукцию для отраслей и сфер потребления - таких как радиоэлектроника, приборостроение, связь, авиационная техника, строительство, телекоммуникация и электротехника.  Мы всегда рады новым проектам и партнёрам! Изготавливаем кабель и провод по ГОСТу, принимаем заказы на разработку и производство по Вашим ТЗ и ТУ, всегда готовы предоставить консультацию по любым вопросам.
                                 </p>
                             </div>
 
-                            <div class="col-md-2 col-lg-2 col-xl-1 mx-auto mb-4" style={{ whiteSpace: 'nowrap' }}>
+                            <div class="col-md-2 col-lg-2 col-xl-1 mx-auto mb-4 footer-links" style={{ whiteSpace: 'nowrap' }}>
                                 {/* <a href="#!" class="text-reset company">Компания</a>
                                 <br />
                                 <a href="#!" class="text-reset">Производство</a>
@@ -43,7 +43,7 @@ export default function Footer() {
                                 <p><a href="#!" class="text-reset">Контакты</a></p>
                             </div>
 
-                            <div class="col-md-2 col-lg-2 col-xl-1 mx-auto mb-4" style={{ whiteSpace: 'nowrap' }}>
+                            <div class="col-md-2 col-lg-2 col-xl-1 mx-auto mb-4 footer-links" style={{ whiteSpace: 'nowrap' }}>
                                 <p><a href="#!" class="text-reset">Наши клиенты</a></p>
                                 <p><a href="#!" class="text-reset">История</a></p>
                                 <p><a href="#!" class="text-reset">Пресс-Центр</a></p>
@@ -52,7 +52,7 @@ export default function Footer() {
                                 <p><a href="#!" class="text-reset">Контакты</a></p>
                             </div>
 
-                            <div class="col-md-2 col-lg-2 col-xl-1 mx-auto mb-4 linkBlock" style={{ whiteSpace: 'nowrap' }}>
+                            <div class="col-md-2 col-lg-2 col-xl-1 mx-auto mb-4 linkBlock footer-links" style={{ whiteSpace: 'nowrap' }}>
                                 <p><a href="#!" class="text-reset">Приёмная</a></p>
                                 <p><a href="#!" class="text-reset">+375-236-24-77-29</a></p>
                                 <br />
@@ -60,7 +60,7 @@ export default function Footer() {
                                 <p><a href="#!" class="text-reset">800 123 12 12</a></p>
                             </div>
                         </div>
-                        <div class="col-md col-lg-4 mx-2 p-4" style={{ color: 'white' }}>
+                        <div class="col-md col-lg-4 mx-2 p-4 footer-links" style={{ color: 'white' }}>
                             © 2021
                         </div>
                     </div>
