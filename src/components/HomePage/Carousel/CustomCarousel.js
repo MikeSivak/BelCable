dab03149d7676f7c191852cbffbfc3261efb0844
MikeSivak <noizemcnorm@gmail.com>
2021-09-01 10:52:31 +0300

    new: change pics in carousel on mobile version

--- src/components/HomePage/Carousel/CustomCarousel.js
+++ src/components/HomePage/Carousel/CustomCarousel.js
@@ -10,6 +10,7 @@ export default function CustomCarousel() {
                 <Carousel>
                     <Carousel.Item>
                         <img className="frontPic" src="h1.png" alt="" />
+                        <img className="frontPic-mobile" src="mc1.png" alt="" />
                         <Carousel.Caption>
                             <div>
                                 <p>Кабели для внутренних и наружных соединений подвижного состава РЕЛЬСОВОГО ТРАНСПОРТА И ТРОЛЛЕЙБУСОВ</p>
@@ -19,6 +20,7 @@ export default function CustomCarousel() {
                     </Carousel.Item>
                     <Carousel.Item>
                         <img className="frontPic" src="h2.png" alt="" />
+                        <img className="frontPic-mobile" src="mc2.png" alt="" />
                         <Carousel.Caption>
                             <div>
                                 <p>КАБЕЛИ И ПРОВОДА СВЯЗИ</p>
@@ -29,45 +31,65 @@ export default function CustomCarousel() {
                     </Carousel.Item>
                     <Carousel.Item>
                         <img className="frontPic" src="h3.png" alt="" />
+                        <img className="frontPic-mobile" src="mc3.png" alt="" />
                         <Carousel.Caption>
                             <div>
-                                <p style={{ maxWidth: '50%' }}>КАБЕЛИ И ПРОВОДА МОНТАЖНЫЕ</p>
+                                <p style={{ maxWidth: '100%' }}>КАБЕЛИ И ПРОВОДА МОНТАЖНЫЕ</p>
+                                <h5 style={{ maxWidth: '100%' }}>
+                                    Предназначены для передачи электрической энергии в пределах одной электрической установки, электрического прибора или аппарата.
+                                </h5>
                                 <Button className='photoButton' variant="danger">Перейти</Button>
                             </div>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img className="frontPic" src="h4.png" alt="" />
+                        <img className="frontPic-mobile" src="mc4.png" alt="" />
                         <Carousel.Caption>
                             <div>
-                                <p style={{ maxWidth: '49%' }}>КАБЕЛИ УПРАВЛЕНИЯ И ПЕРЕДАЧИ ДАННЫХ</p>
+                                <p style={{ maxWidth: '100%' }}>КАБЕЛИ УПРАВЛЕНИЯ И ПЕРЕДАЧИ ДАННЫХ</p>
+                                <h5 style={{ maxWidth: '70%' }}>
+                                    Предназначены для передачи сигналов к аппаратуре контроля.
+                                </h5>
                                 <Button className='photoButton' variant="danger">Перейти</Button>
                             </div>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img className="frontPic" src="h5.png" alt="" />
+                        <img className="frontPic-mobile" src="mc5.png" alt="" />
                         <Carousel.Caption>
                             <div>
                                 <p style={{ maxWidth: '49%' }}>КАБЕЛИ СИЛОВЫЕ</p>
+                                <h5 style={{ maxWidth: '90%' }}>
+                                    Для передачи электроэнергии токами промышленных частот и для стационарной прокладки.
+                                </h5>
                                 <Button className='photoButton' variant="danger">Перейти</Button>
                             </div>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img className="frontPic" src="h6.png" alt="" />
+                        <img className="frontPic-mobile" src="mc6.png" alt="" />
                         <Carousel.Caption>
                             <div>
                                 <p style={{ maxWidth: '80%' }}>КАБЕЛИ И ПРОВОДА ДЛЯ СИГНАЛИЗАЦИИ И БЛОКИРОВКИ</p>
+                                <h5 style={{ maxWidth: '90%' }}>
+                                    Для монтажа систем сигнализации и эксплуатации в условиях стационарной и нестационарной прокладки
+                                </h5>
                                 <Button className='photoButton' variant="danger">Перейти</Button>
                             </div>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img className="frontPic" src="h7.png" alt="" />
+                        <img className="frontPic-mobile" src="mc7.png" alt="" />
                         <Carousel.Caption>
-                            <div style={{ marginTop: '70px' }}>
+                            <div>
                                 <p>КАБЕЛИ КОНТРОЛЬНЫЕ</p>
+                                <h5 style={{ maxWidth: '90%' }}>
+                                    Для неподвижного присоединения к электрическим приборам, аппаратам, сборкам зажимов электрических распределительных устройств
+                                </h5>
                                 <Button className='photoButton' variant="danger">Перейти</Button>
                             </div>
                         </Carousel.Caption>
