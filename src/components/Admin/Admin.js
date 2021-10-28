873609d818e7381482085463d5a78bea89f3fd4b
MikeSivak <noizemcnorm@gmail.com>
2021-10-28 20:09:24 +0300

    add tabs and inputs on admin page

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -8,19 +8,73 @@ export default function Admin() {
 
                 <Tabs defaultActiveKey="add" id="uncontrolled-tab-example" className="mb-3">
                     <Tab eventKey="add" title="Добавление">
-                        <span className='action-label'>Добавление</span>
                         <div className='add-product-container'>
                             {/* add product with all fields */}
                             <div className='add-product-section'>
+                                <span className='action-label'>Добавление товара</span>
+                                <Form className='add-product-form'>
+                                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                                        <br></br>
+                                        <Form.Control type="text" placeholder="Имя товара" />
+                                        <br></br>
+                                        <Form.Control type="text" placeholder="Описание оболочки" />
+                                        <br></br>
+                                        <Form.Control type="text" placeholder="Назначение товара" />
+                                        <br></br>
+                                        <Form.Control type="text" placeholder="Конструкция товара" />
+                                        <br></br>
+                                        <Form.Control type="text" placeholder="Материал трубки" />
+                                        <br></br>
+                                        <Form.Control type="text" placeholder="Характеристики экрана" />
+                                        <br></br>
+                                        <Form.Control type="text" placeholder="Материал изоляции" />
+                                        <br></br>
+                                        <Form.Control type="text" placeholder="Материал оболочки" />
+                                        <br></br>
+                                        <Form.Control type="text" placeholder="С экраном? (Да/Нет)" />
+                                        <br></br>
+                                        <br></br>
+                                        <div>
+                                            <Form.Label>Технические Характеристики</Form.Label>
+                                            <br></br>
+                                            <h4>Нажмите на кнопку для добавления новой характеристики товара</h4>
+                                            <br></br>
+                                            <Button style={{ borderRadius: '100%', height: '50px', width: '50px' }}>
 
+                                            </Button>
+                                        </div>
+                                    </Form.Group>
+                                    <Button style={{marginTop:'20px', width:'50%'}} variant="primary" type="submit">
+                                        Добавить
+                                    </Button>
+                                    <br></br>
+                                    <br></br>
+                                </Form>
                             </div>
+                            
                             {/* add category only */}
                             <div className='add-category-section'>
-
+                                <Form>
+                                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                                        <Form.Label>Добавить категорию</Form.Label>
+                                        <Form.Control type="text" placeholder="Введите имя категории" />
+                                    </Form.Group>
+                                    <Button variant="primary" type="submit">
+                                        Добавить
+                                    </Button>
+                                </Form>
                             </div>
                             {/* add industry only */}
                             <div className='add-industry-section'>
-
+                                <Form>
+                                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                                        <Form.Label>Добавить отрасль</Form.Label>
+                                        <Form.Control type="text" placeholder="Введите имя отрасли" />
+                                    </Form.Group>
+                                    <Button variant="primary" type="submit">
+                                        Добавить
+                                    </Button>
+                                </Form>
                             </div>
                         </div>
                     </Tab>
